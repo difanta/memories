159ebf9b946e2b9b2c2b19f81f5325eff6b27abf
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2026-03-25 23:12:41 +0100

    NC33 - multipreview - reduce number of database queries by obtaining all available previews for requested files
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>

--- lib/Controller/ImageController.php
+++ lib/Controller/ImageController.php
@@ -122,6 +122,9 @@ final class ImageController extends GenericApiController
             /** @var \OC\Preview\PreviewService */
             $previewService = \OC::$server->get(\OC\Preview\PreviewService::class);
 
+            $requestedFileIds = array_map(static fn ($bodyFile) => (int) $bodyFile['fileid'], $files);
+            $availablePreviews = $previewService->getAvailablePreviews($requestedFileIds);
+
             // stream the response
             $out->setHeader('Content-Type: application/octet-stream');
 
@@ -135,7 +138,7 @@ final class ImageController extends GenericApiController
                 try {
                     // Make sure max preview exists
                     $hasMax = false;
-                    foreach ($previewService->getAvailablePreviewsForFile($fileid) as $preview) {
+                    foreach ($availablePreviews[$fileid] as $preview) {
                         if ($preview->isMax()) {
                             $hasMax = true;
 
